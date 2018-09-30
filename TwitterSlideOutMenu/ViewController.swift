fa481c0a436cf51d4b5feb085ae612348e11ca8b
Vandan Patel <vandan.patel@nike.com>
2018-09-29 22:51:07 -0700

    Bypasses Storyboard and creates dummy tableView.

--- TwitterSlideOutMenu/ViewController.swift
+++ TwitterSlideOutMenu/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.backgroundColor = .red
+        setupNavigationItems()
+    }
+
+    @objc func handleOpen() {
+        print("Opening Menu ...")
+    }
+
+    @objc func handleHide() {
+        print("Hiding Menu ...")
     }
 
 
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem    = UIBarButtonItem(title: "Open",
+                                                              style: .plain,
+                                                              target: self,
+                                                              action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem   = UIBarButtonItem(title: "Hide",
+                                                              style: .plain,
+                                                              target: self,
+                                                              action: #selector(handleHide))
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellID")
+        cell.textLabel?.text = "Row: \(indexPath.row)"
+        return cell
+    }
 }
 
