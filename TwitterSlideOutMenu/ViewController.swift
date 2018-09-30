dcc5f510bacc95252492697e17aca5256bbaf368
Vandan Patel <vandan.patel@nike.com>
2018-09-29 19:58:07 -0700

    Initial Commit

---
+++ TwitterSlideOutMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterSlideOutMenu
+//
+//  Created by Patel, Vandan (ETW - FLEX) on 9/29/18.
+//  Copyright © 2018 Patel, Vandan (ETW - FLEX). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
